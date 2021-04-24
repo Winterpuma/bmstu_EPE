fa8a838fc90f839daebaedd0ecbf2830f361db58
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2021-04-24 04:19:48 +0300

    Add enum description

--- lab_6/var_1/COCOMO_var1/Attributes/Ratings.cs
+++ lab_6/var_1/COCOMO_var1/Attributes/Ratings.cs
@@ -1,12 +1,20 @@
-﻿namespace COCOMO.Attributes
+﻿using System.ComponentModel;
+
+namespace COCOMO.Attributes
 {
 	public enum Ratings
 	{
+		[Description("Очень низкий")]
 		VeryLow = -2,
+		[Description("Низкий")]
 		Low = -1,
+		[Description("Номинальный")]
 		Nominal = 0,
+		[Description("Высокий")]
 		High = 1,
+		[Description("Очень высокий")]
 		VeryHigh = 2,
+		[Description("Экстра высокий")]
 		ExtraHigh = 3
 	}
 }
